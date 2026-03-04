ed5c52e6814c69eecd188b4ff59651be4d195d4c
Osmar <tu_correo@ejemplo.com>
2026-03-04 19:54:08 -0400

    PUT Y DELETE DE TABLA PERSONA

--- routes/materiales.py
+++ routes/materiales.py
@@ -37,23 +37,3 @@ async def insertar(mat: Material, conn = Depends(get_conexion)):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.put("/{id_material}")
-async def actualizar(id_material: int, mat: Material, conn = Depends(get_conexion)):
-    consulta = "UPDATE materiales SET nombre_componente=%s, costo_unitario=%s, id_proveedor=%s WHERE id_material=%s"
-    try:
-        async with conn.cursor() as cursor:
-            await cursor.execute(consulta, (mat.nombre_componente, mat.costo_unitario, mat.id_proveedor, id_material))
-            await conn.commit()
-            return {"mensaje": "Material Actualizado"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-@router.delete("/{id_material}")
-async def eliminar(id_material: int, conn = Depends(get_conexion)):
-    try:
-        async with conn.cursor() as cursor:
-            await cursor.execute("DELETE FROM materiales WHERE id_material = %s", (id_material,))
-            await conn.commit()
-            return {"mensaje": "Material Eliminado"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
