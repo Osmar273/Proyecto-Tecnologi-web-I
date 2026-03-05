c5f971ba0ca8c3ec1127b417f45f8143c5a3b98b
Osmar <tu_correo@ejemplo.com>
2026-03-04 20:11:09 -0400

    ARCHIVO CONEXION.PY CON NUEVO CODIGO

--- config/conexionBD.py
+++ config/conexionBD.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
 from psycopg_pool import AsyncConnectionPool
 from psycopg.rows import dict_row
+from config.configuracion import config
 from contextlib import asynccontextmanager
 
-DB_URL = "postgresql://postgres:1234@localhost:5432/bd_proyecto"
+DB_URL = f"postgresql://{config.db_user}:{config.db_pass}@{config.db_host}:{config.db_port}/{config.db_name}"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
     app.async_pool = AsyncConnectionPool(conninfo=DB_URL, open=False)
     try:
         await app.async_pool.open()
-        print("Pool de conexiones abierto exitosamente")
+        print(f"Conectado a la base de datos: {config.db_name} (Pool abierto)")
         yield
     finally:
-        await app.async_pool.close() 
-        print("Pool de conexiones cerrado")
+        await app.async_pool.close()
+        print("Pool de conexiones cerrado correctamente")
 
 app = FastAPI(lifespan=lifespan)
 
